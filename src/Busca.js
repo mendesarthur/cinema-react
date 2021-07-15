3b51b9b277b5520cfb90196bae3d0d927821d4e1
Your Name <you@example.com>
2021-07-14 21:10:07 -0300

    ..

--- src/Busca.js
+++ src/Busca.js
@@ -21,7 +21,7 @@ function Busca() {
 				<span className="title">Matrix</span>
 				<span className="year">1993</span>
 			</li>
-			
+
 			<li>
 				<img className="poster" src="https://m.media-amazon.com/images/M/MV5BYzUzOTA5ZTMtMTdlZS00MmQ5LWFmNjEtMjE5MTczN2RjNjE3XkEyXkFqcGdeQXVyNTc2ODIyMzY@._V1_SX300.jpg" alt="Poster" />
 				<span className="title">Matrix</span>
@@ -35,20 +35,20 @@ function Busca() {
 			
 			<li>
 			<img className="poster" src="https://m.media-amazon.com/images/M/MV5BMzg2Mjg1OTk0NF5BMl5BanBnXkFtZTcwMjQ4MTA3Mw@@._V1_SX300.jpg" alt="Poster" />
-			<span className="title"></span>
-			<span className="year"></span>
+			<span className="title">redx</span>
+			<span className="year">1998</span>
 			</li>
 
 			<li>
 			<img className="poster" src="https://m.media-amazon.com/images/M/MV5BMzg2Mjg1OTk0NF5BMl5BanBnXkFtZTcwMjQ4MTA3Mw@@._V1_SX300.jpg" alt="Poster" />
-			<span className="title"></span>
-			<span className="year"></span>
+			<span className="title">redx</span>
+			<span className="year">1998</span>
 			</li>
 
 			<li>
 			<img className="poster" src="https://m.media-amazon.com/images/M/MV5BMzg2Mjg1OTk0NF5BMl5BanBnXkFtZTcwMjQ4MTA3Mw@@._V1_SX300.jpg" alt="Poster" />
-			<span className="title"></span>
-			<span className="year"></span>
+			<span className="title">redx</span>
+			<span className="year">1998</span>
 			</li>
 
 		</ul>
